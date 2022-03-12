89d1fce24dcacf2822f9b99ffeead0993f83478a
Sonali <sonali@Sonalis-MacBook-Air.local>
2022-03-12 13:50:23 +0530

    starter template

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      This is Header
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Login/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+  return (
+    <div className="Login">
+      Start working here
+    </div>
+  );
+}
+
+export default Login;
