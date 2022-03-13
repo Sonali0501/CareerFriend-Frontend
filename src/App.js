ebb4e9ed1fca0f7c66b088c20e20e114cbf734f0
Srishti <48403800+srishti011@users.noreply.github.com>
2022-03-13 08:43:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from 'react';
+import MenuItem from "@material-ui/core/MenuItem";
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
 import Header from './components/Header';
 import Login from './components/Login';
+import Form from './components/Form';
+import Profile from './components/Profile';
 import './App.css';
 
 const App = () => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Login />
+       <Login />
+       <Header />
+       <Form />
+       <Profile />
+       style={{
+        marginLeft: "40%",
+      }}
+    
+      <h2>Menu</h2>
+      <Button
+        aria-controls="simple-menu"
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        Open Menu List
+      </Button>
+      <Menu
+        keepMounted
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        open={Boolean(anchorEl)}
+      >
+        <MenuItem onClick={handleClose}>My Account</MenuItem>
+        <MenuItem onClick={handleClose}>Settings</MenuItem>    
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
     </div>
   );
 }
